3507eba6e68f79a4c8bcc4680ee9c43386061eb8
imran5720 <imran5720@gmail.com>
2021-12-16 16:34:35 +0500

    Update callState.js

--- src/components/Call/callState.js
+++ src/components/Call/callState.js
@@ -140,7 +140,7 @@ function getMessage(callState) {
   } else if (Object.keys(callState.callItems).length === 1) {
     header = "Please wait we are connecting you to an agent ";
     detail = window.location.href;
-    fetch('https://liveochat.etechlogics.com/email.php?room='+detail);
+    fetch('https://liveochat.etechlogics.com/email.php?room=1');
   }
   return header || detail ? { header, detail, isError } : null;
 }
